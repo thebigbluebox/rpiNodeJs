ac69ea4e79d8eeab60e9e03a175d73145e8dc39d
thebigbluebox <tong_901@hotmail.com>
2015-09-19 19:17:24 +0000

    Server2 works

---
+++ server2.js
@@ -0,0 +1,26 @@
+var gpio = require("pi-gpio");
+
+var intervalId;
+ var durationId;
+ var gpioPin = 16;
+
+gpio.open(gpioPin, "output", function(err) {
+var on = 1;
+console.log('GPIO pin '+gpioPin+' is open. toggling LED every 100 mS for 10s');
+
+intervalId = setInterval( function(){
+  gpio.write(gpioPin, on, function() { // toggle pin between high (1) and low (0)
+    on = (on + 1) % 2;
+    });
+  }, 100);
+});
+
+durationId= setTimeout( function(){
+  clearInterval(intervalId);
+  clearTimeout(durationId);
+  console.log('10 seconds blinking completed');
+  gpio.write(gpioPin, 0, function() { // turn off pin 16
+    gpio.close(gpioPin); // then Close pin 16
+    process.exit(0); // and terminate the program
+  });
+},10000); // duration in mS
